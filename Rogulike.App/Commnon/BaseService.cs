f0238c1d5772b6b79bd15d2669efeb029da21265
Michal-Hildebrandt <84286254+Michal-Hildebrandt@users.noreply.github.com>
2022-01-15 18:09:10 +0100

    Add files via upload

--- Rogulike.App/Commnon/BaseService.cs
+++ Rogulike.App/Commnon/BaseService.cs
@@ -3,11 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Rogulike.App.Abstract;
+using Roguelike.Domain.Common;
+using Roguelike.App.Abstract;
 
-namespace Rogulike.App.Commnon
+namespace Roguelike.App.Commnon
 {
-    public class  BaseService<T> : IService<T>
+    public class BaseService<T> : IService<T> where T : BaseEntity
     {
+        public List<T> Items { get; set; }
+        public BaseService()
+        {
+            Items = new List<T>();
+        }
+
+        public virtual int CreateItem(T item)
+        {
+            Items.Add(item);
+            return item.Id;
+        }
+
+        public List<T> GetAllItems()
+        {
+            return Items;
+        }
+
+        public void RemoveItem(T item)
+        {
+            Items.Remove(item);
+        }
+
+        public int UpdateItem(T item)
+        {
+            var entity = Items.FirstOrDefault(x => x.Id == item.Id);
+            if (entity != null)
+            {
+                entity = item;
+            }
+
+            return entity.Id;
+        }
     }
 }
