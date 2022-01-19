fe46ff323e328b143b357021db1c8b2f71780817
Michal-Hildebrandt <84286254+Michal-Hildebrandt@users.noreply.github.com>
2022-01-19 22:37:00 +0100

    Refactoring

--- Rogulike.App/Concrete/ExperienceService.cs
+++ Rogulike.App/Concrete/ExperienceService.cs
@@ -1,4 +1,5 @@
-﻿using Roguelike.Domain.Entity;
+﻿using Roguelike.App.Managers;
+using Roguelike.Domain.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,85 +10,51 @@ namespace Roguelike
 {
     public class ExperienceService:SkillsService
     {
-        public ChosenClass CalculateExperience(EnemyGenerator enemy, Experience experience, ChosenClass result, Skills skill)
+        public ChosenClass CalculateExperience(EnemyGenerator enemy, Experience experience, ChosenClass result)
         {
-            experience.ExpRequired = new int[30];
-            experience.ExpRequired[0] = 0;
-            for (int i = 0; i < 30; i++)
-            {
-                experience.ExpRequired[i] = 35 * i;
-            }
-
             experience.TotalExp += enemy.Exp;
+            return result;
+        }
+        public ChosenClass CalculateExperience(Boss bossStats, Experience experience, ChosenClass result)
+        {
+            return result;
+        }
 
+        public ChosenClass LevelUp(ChosenClass result, Experience experience, ExperienceManager experienceManager, Skills skill, ExperienceService experienceService)
+        {
 
+            bool isGreater = experience.TotalExp >= experience.ExpRequired[result.Level + 1];
 
-           while(true)
+            if (result.Level <30)
             {
-                if (experience.TotalExp >= experience.ExpRequired[result.Level+1] && result.Level < 30)
+                if (isGreater == true)
                 {
                     result.Level += 1;
                     experience.UpgradePoints += 1;
                     experience.TotalExp = experience.TotalExp - experience.ExpRequired[result.Level];
-                    Console.WriteLine("You have advanced to " + result.Level + " level\n");
-                    Console.WriteLine("You have " + experience.UpgradePoints + " upgrade points to spend\n");
-                    Console.WriteLine("Choose which stat you want to upgrade or skip this time to accumulate points ");
-                    Console.WriteLine("Press 1 (spend points), 2 (unlock skill - costs 3 upgrade points), 3 (go further without any upgrade and keep points)\n");
-                    
-                    var operation = Console.ReadKey();
-                    Console.WriteLine();
-                    int.TryParse(operation.KeyChar.ToString(), out int val);
-
-                    switch (val)
-                    {
-                        case 1:
-           
-                            Console.WriteLine(" Currently you have " + result.Hp + " upgrade health (+10 HP - press 1)");
-                            Console.WriteLine(" Currently you have " + result.Damage + " upgrade damage (+5 Dmg - press 2)");
-
-                            var operation1 = Console.ReadKey();
-                            Console.WriteLine();
-                            int val1;
-                            int.TryParse(operation1.KeyChar.ToString(), out val1);
-
-                            switch (val1)
-                            {
-                                case 1:
-                                    result.Hp += 10;
-                                    experience.UpgradePoints -= 1;
-                                    break;
-                                case 2:
-                                    result.Damage += 5;
-                                    experience.UpgradePoints -= 1;
-                                    break;
-                            }
-                            break;
-                        case 2:
-                            if (experience.UpgradePoints >= 3)
-                            {
-                                Console.WriteLine("You've unlocked skill: " + skill.Name);
-                                skill.IsLocked = false;
-                                experience.UpgradePoints -= 3;
-                            }
-                            else
-                            {
-                                Console.WriteLine("You don't have that much points to unlock skill !");
-                            }
-                        break;
-                    }
-                    continue;
-                }
-                else if (result.Level == 30)
-                {
-                    Console.WriteLine("You've reached max level ! You have " + experience.TotalExp);
-                }
-                else
-                {
-                    Console.WriteLine("You have " + experience.TotalExp + " / " + experience.ExpRequired[result.Level+1] + " exp\n");
-                    break;
+                    experienceManager.GetLeveulUpMenu(result, experience, skill, experienceService);
                 }
             }
+            else
+            {
+                experienceManager.MaxLvlWarning(experience);
+            }
+            
             return result;
         }
-    }
+        
+        public ChosenClass IncreaseHp(ChosenClass result, Experience experience)
+        {
+            result.Hp += 10;
+            experience.UpgradePoints -= 1;
+            return result;
+        }
+
+        public ChosenClass IncreaseDmg(ChosenClass result, Experience experience)
+        {
+            result.Damage += 5;
+            experience.UpgradePoints -= 1;
+            return result;
+        }
+    }   
 }
