7e2565308727aa65c35b75435607671780466703
Michal-Hildebrandt <84286254+Michal-Hildebrandt@users.noreply.github.com>
2022-01-23 16:13:09 +0100

    Added managers in the scope of refactoring process

---
+++ Rogulike.App/Managers/ChosenClassManager.cs
@@ -0,0 +1,30 @@
+﻿using Roguelike.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roguelike.App.Managers
+{
+    public class ChosenClassManager
+    {
+        public bool  Confirmation(ChosenClass result )
+        {
+            bool confirm;
+            Console.WriteLine("Do you want to start as " + result.Name.ToString() + " ? (y/n) \n ");
+
+            var confirmation = Console.ReadKey().KeyChar;
+            if (confirmation == 'y')
+            {
+                confirm = true;
+            }
+            else
+            {
+                confirm = false;
+            }
+            Console.WriteLine();
+            Console.Clear();
+
+            return confirm;
+        }
+    }
+}
---
+++ Rogulike.App/Managers/ScoreManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Roguelike.App.Managers
+{
+    public class ScoreManager
+    {
+        public void Highscore(int floor)
+        {
+            Console.WriteLine("You've completed " + floor + " floor/-s \n");
+            Console.WriteLine("Type your nickname: ");
+            var playerNickname = Console.ReadLine();
+            TextWriter score = new StreamWriter(@"D:\Highscore.txt", true);
+            score.WriteLine("Number of completed floors: " + floor + ", by " + playerNickname.ToString());
+            score.Close();
+        }
+       
+    }
+}
