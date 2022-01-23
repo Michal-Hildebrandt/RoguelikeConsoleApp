69234125f159e22ff107799c47c176f9fef367ec
Michal-Hildebrandt <84286254+Michal-Hildebrandt@users.noreply.github.com>
2022-01-23 17:24:27 +0100

    Add files via upload

--- Rogulike.App/Managers/ChosenClassManager.cs
+++ Rogulike.App/Managers/ChosenClassManager.cs
@@ -1,13 +1,31 @@
 ﻿using Roguelike.Domain.Entity;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Roguelike.App.Managers
 {
     public class ChosenClassManager
     {
-        public bool  Confirmation(ChosenClass result )
+        public int ChoosingClass(MenuActionService actionService)
+        {
+            var choosingClass = actionService.GetMenuActionsByMenuName("Choosing Class");
+            Console.WriteLine("Choose your class: ");
+            for (int i = 0; i < choosingClass.Count; i++)
+            {
+                Console.WriteLine($"{choosingClass[i].Id}. {choosingClass[i].Name}");
+            }
+
+            var operation1 = Console.ReadKey();
+            int val;
+            int.TryParse(operation1.KeyChar.ToString(), out val);
+            Console.WriteLine();
+
+            return val;
+
+        }
+        public bool Confirmation(ChosenClass result )
         {
             bool confirm;
             Console.WriteLine("Do you want to start as " + result.Name.ToString() + " ? (y/n) \n ");
