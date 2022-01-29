621fef1623970b5bfbc805ffbc899aaafa4b847a
Michal-Hildebrandt <84286254+Michal-Hildebrandt@users.noreply.github.com>
2022-01-29 21:30:35 +0100

    Add files via upload

---
+++ Roguelike.Test/Services/Boss.cs
@@ -0,0 +1,25 @@
+﻿using Roguelike.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roguelike
+{
+    public class Boss:EnemyGenerator
+    {
+        public string Name { get; set; }
+
+        public Boss()
+        {
+
+        }
+        public Boss(int id, int hp, int damage, int exp, string name)
+        {
+            Id = id;
+            Hp = hp;
+            Damage = damage;
+            Exp = exp;
+            Name = name;
+        }
+    }
+}
