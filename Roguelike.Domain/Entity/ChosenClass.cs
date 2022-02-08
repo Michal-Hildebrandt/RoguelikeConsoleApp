ebcaec1c6da50d55afaff75ec46eb65ba9c3bc61
Michal-Hildebrandt <84286254+Michal-Hildebrandt@users.noreply.github.com>
2022-02-08 23:28:17 +0100

    Add files via upload

--- Roguelike.Domain/Entity/ChosenClass.cs
+++ Roguelike.Domain/Entity/ChosenClass.cs
@@ -14,18 +14,24 @@ namespace Roguelike.Domain.Entity
         public int Hp { get; set; }
         public int Damage { get; set; }
         public int Level {get; set; }
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
 
+        public char Symbol { get; set; }
         public ChosenClass()
         {
 
         }
-        public ChosenClass(int id, string name, int hp, int damage, int level)
+        public ChosenClass(int id, string name, int hp, int damage, int level, int positionX, int positionY, char symbol)
         {
             Id = id;
             Name = name;
             Hp = hp;
             Damage = damage;
             Level = level;
+            PositionX = positionX;
+            PositionY = positionY;
+            Symbol = symbol;
         }
     }
 
